1bf8213aa5bb54767cd9fe1760a6a9f32d320272
Max Keneau <gart@gart.sh>
2026-03-03 18:28:47 -0800

    update parmesan recipe

--- src/client/foods/pe.tsx
+++ src/client/foods/pe.tsx
@@ -3,15 +3,43 @@ export function render() {
 	return (
 		<div>
 			<h1>chicken parmesan</h1>
+			<p>
+				we ended up following{" "}
+				<a href="https://www.budgetbytes.com/chicken-parmesan/" target="_blank">
+					this recipe
+				</a>
+				!
+			</p>
 
 			<h2>ingredients</h2>
-
 			<ul>
-				<li>@TODO put ingredients here...</li>
+				<li>boneless, skinless chicken breast</li>
+				<li>breadcrumbs</li>
+				<li>parmesan cheese</li>
+				<li>garlic powder</li>
+				<li>Italian seasoning</li>
+				<li>salt</li>
+				<li>pepper</li>
+				<li>egg</li>
+				<li>all-purpose flour</li>
+				<li>olive oil/avacado oil (we used both)</li>
+				<li>marinara sauce</li>
+				<li>shredded mozzarella</li>
+				<li>chopped fresh parsley</li>
+
+				<li>kroger garlic bread</li>
+				<li>frozen corn</li>
+				<li>butter</li>
+			</ul>
+
+			<h3>includes</h3>
+			<ul>
+				<li>gluten</li>
+				<li>milk</li>
+				<li>egg</li>
 			</ul>
 
 			<h2>reheat instructions</h2>
-
 			<p>unclip lid, and set on top of container, leaving space to vent. put in microwave for 90 seconds.</p>
 		</div>
 	);
